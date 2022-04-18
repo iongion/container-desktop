cf310387e02eb5aa55deea91a7ccfa532001dad5
ionut stoica <stoica.ionut@gmail.com>
2022-04-18 10:08:59 +0200

    Binaries for ARM arch

--- tasks.py
+++ tasks.py
@@ -42,12 +42,13 @@ def bundle_apps(c, env=None):
     system = platform.system()
     with c.cd("src/app"):
         if system == "Darwin":
-            run_env(c, "npm run package:mac", env)
+            run_env(c, "npm run package:mac_x86", env)
+            run_env(c, "npm run package:mac_arm", env)
         elif system == "Linux":
             run_env(c, "npm run package:linux_x86", env)
-            # run_env(c, "npm run package:linux_arm", env)
+            run_env(c, "npm run package:linux_arm", env)
         else:
-            run_env(c, "npm run package:win", env)
+            run_env(c, "npm run package:win_x86", env)
 
 
 @task(default=True)
