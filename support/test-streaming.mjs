f64c2fff36700e0e9abdbe28a20993a8c9f1b2c0
Ionut Stoica <stoica.ionut@gmail.com>
2024-10-07 12:10:47 +0200

    Add events streaming test

---
+++ support/test-streaming.mjs
@@ -0,0 +1,28 @@
+import axios from "axios";
+import httpAdapter from "axios/lib/adapters/http.js";
+import http from "http";
+
+export function createApiDriver() {
+  const httpAgent = new http.Agent({
+    keepAlive: true,
+    keepAliveMsecs: 10,
+    timeout: 30000
+  });
+  const driver = axios.create({
+    adapter: httpAdapter,
+    httpAgent,
+    baseURL: "http://d",
+    // socketPath: "\\\\.\\pipe\\podman-machine-default"
+    // socketPath: "/run/user/1000/podman/podman.sock",
+    socketPath: "/var/run/docker.sock"
+  });
+  return driver;
+}
+
+const driver = createApiDriver();
+console.debug(">> PING");
+let response = await driver.get("/_ping");
+console.debug("<< PING", response.data);
+console.debug(">> EVENTS STREAM");
+response = await driver.get("/events", { responseType: "stream" });
+console.debug("<< EVENTS STREAM", response);
